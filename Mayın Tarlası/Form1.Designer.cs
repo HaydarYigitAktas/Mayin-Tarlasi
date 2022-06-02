4303019f783d0d6461e9132a8ce26953e6212eb1
HaydarYigitAktas <106775118+HaydarYigitAktas@users.noreply.github.com>
2022-06-02 23:24:40 +0300

    c# mayın tarlası oyunu
    C# Form uygulamasında kodlamış olduğum mayın tarlası oyunu.

---
+++ Mayın Tarlası/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Mayın_Tarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(95, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "YENİDEN BAŞLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BAYRAK";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SÜRE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 420);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Mayın Tarlası/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Mayın_Tarlası
+{
+    public partial class Form1 : Form
+    {
+        Random Rasgele = new Random();
+
+        byte BombaSayısı = 10;
+        Button[] Duğmeler = new Button[64];
+        DataTable MayınTarlası = new DataTable();
+        
+
+        public Form1()
+        {
+
+            başlat();
+
+        }
+        void başlat()
+        {
+            tarlaEkimi();
+
+            InitializeComponent();
+
+            TabloDoldur();
+
+            sayıYerlestirme();
+
+        }
+        
+
+        void tarlaEkimi()
+        {
+            int horizotal = 30;
+            int vertical = 100;
+            byte butonİsim = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int a = 0; a < 8; a++)
+                {
+                    Duğmeler[butonİsim] = new Button();
+                    Duğmeler[butonİsim].Name = butonİsim.ToString();
+                    Duğmeler[butonİsim].Size = new Size(30, 30);
+                    Duğmeler[butonİsim].Location = new Point(horizotal, vertical);
+                    Duğmeler[butonİsim].MouseDown += new MouseEventHandler(this.Duğmeler_MouseDown);
+                    Duğmeler[butonİsim].BackColor = Color.Aqua;
+
+
+                    horizotal += 30;
+                    this.Controls.Add(Duğmeler[butonİsim]);
+
+                    butonİsim++;
+                }
+                horizotal = 30;
+                vertical += 30;
+            }
+
+        }
+
+        void sayıYerlestirme()
+        {
+            byte bombaSayısı = 0;
+
+            for (int SatırSırası = 1; SatırSırası < 9; SatırSırası++)
+            {
+                for (int SütunSırası = 1; SütunSırası < 9; SütunSırası++)
+                {
+                    if (MayınTarlası.Rows[SatırSırası][SütunSırası].ToString() != "255")
+                    {
+
+                        if (MayınTarlası.Rows[SatırSırası - 1][Convert.ToString(SütunSırası - 1)].ToString() == "255")
+                            bombaSayısı += 1;
+                        if (MayınTarlası.Rows[SatırSırası - 1][Convert.ToString(SütunSırası)].ToString() == "255")
+                            bombaSayısı += 1;
+                        if (MayınTarlası.Rows[SatırSırası - 1][Convert.ToString(SütunSırası + 1)].ToString() == "255")
+                            bombaSayısı += 1;
+
+                        if (MayınTarlası.Rows[SatırSırası][Convert.ToString(SütunSırası - 1)].ToString() == "255")
+                            bombaSayısı += 1;
+                        if (MayınTarlası.Rows[SatırSırası][Convert.ToString(SütunSırası + 1)].ToString() == "255")
+                            bombaSayısı += 1;
+
+                        if (MayınTarlası.Rows[SatırSırası + 1][Convert.ToString(SütunSırası - 1)].ToString() == "255")
+                            bombaSayısı += 1;
+                        if (MayınTarlası.Rows[SatırSırası + 1][Convert.ToString(SütunSırası)].ToString() == "255")
+                            bombaSayısı += 1;
+                        if (MayınTarlası.Rows[SatırSırası + 1][Convert.ToString(SütunSırası + 1)].ToString() == "255")
+                            bombaSayısı += 1;
+
+                        MayınTarlası.Rows[SatırSırası][SütunSırası] = bombaSayısı;
+
+                        bombaSayısı = 0;
+                    }
+
+                }
+
+            }
+        }
+
+        void TabloDoldur()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MayınTarlası.Columns.Add(Convert.ToString(i), typeof(byte));
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                DataRow dr = MayınTarlası.NewRow();
+
+                dr[0] = 0;
+                dr[1] = 0;
+                dr[2] = 0;
+                dr[3] = 0;
+                dr[4] = 0;
+                dr[5] = 0;
+                dr[6] = 0;
+                dr[7] = 0;
+                dr[8] = 0;
+                dr[9] = 0;
+
+                MayınTarlası.Rows.Add(dr);
+
+            }
+
+            bombaYerleştir();
+        }
+
+        List<byte> bombasütunları = new List<byte>();
+        List<byte> bombasatırları = new List<byte>();
+
+        void bombaYerleştir()
+        {
+            byte bombaSatır;
+            byte bombaSütun;
+
+            for (int i = 0; i < BombaSayısı; i++)
+            {
+                bombaSatır = Convert.ToByte(Rasgele.Next(1, 9));
+                bombaSütun = Convert.ToByte(Rasgele.Next(1, 9));
+
+                MayınTarlası.Rows[bombaSatır][bombaSütun] = 255;
+                bombasatırları.Add(bombaSatır);
+                bombasütunları.Add(bombaSütun);
+            }
+        }
+
+        public byte degerBul(byte sıra)
+        {
+
+            byte satır = 1;
+            byte sütun = 1;
+            byte tabloSıra = 0;
+
+            for (byte s = 0; s < 8; s++)
+            {
+
+                for (byte sü = 0; sü < 8; sü++)
+                {
+                    tabloSıra += 1;
+                    if (tabloSıra == sıra)
+                    {
+                        s++;
+                        sü++;
+                        satır = s;
+                        sütun = sü;
+                        break;
+                    }
+                }
+                if (tabloSıra == sıra)
+                {
+                    break;
+                }
+            }
+
+            if (MayınTarlası.Rows[satır][sütun].ToString() != "255")
+            {
+                Duğmeler[sıra - 1].Text = MayınTarlası.Rows[satır][sütun].ToString();
+
+            }
+            else
+            {
+                Duğmeler[sıra - 1].BackColor = Color.Black;
+
+
+            }
+            return Convert.ToByte(MayınTarlası.Rows[satır][sütun]);
+        }
+        void MayınaBastın()
+        {
+            for (byte i = 1; i < 64; i++)
+            {
+                if (degerBul(i) == 255)
+                {
+                    Duğmeler[i].BackColor = Color.Black;
+                }
+            }
+            for (byte i = 1; i < 64; i++)
+            {
+                Duğmeler[i].Enabled = false;
+            }
+
+        }
+        List<byte> sıfırAç(byte a)
+        {
+            List<byte> ÇevredekiSıfırlar = new List<byte>();
+
+            if (a - 9 > 0 && a != 1 && a != 9 && a != 17 && a != 25 && a != 33 && a != 41 && a != 49 && a != 57)
+                if (degerBul(Convert.ToByte(a - 9)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a - 9));
+            if (a - 8 > 0)
+                if (degerBul(Convert.ToByte(a - 8)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a - 8));
+            if (a - 7 > 0 && a != 8 && a != 16 && a != 24 && a != 32 && a != 40 && a != 48 && a != 56 && a != 64)
+                if (degerBul(Convert.ToByte(a - 7)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a - 7));
+            if (a - 1 > 0 && a != 1 && a != 9 && a != 17 && a != 25 && a != 33 && a != 41 && a != 49 && a != 57)
+                if (degerBul(Convert.ToByte(a - 1)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a - 1));
+
+
+            if (a + 1 < 64 && a != 8 && a != 16 && a != 24 && a != 32 && a != 40 && a != 48 && a != 56 && a != 64)
+                if (degerBul(Convert.ToByte(a + 1)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a + 1));
+            if (a + 7 < 64 && a != 1 && a != 9 && a != 17 && a != 25 && a != 33 && a != 41 && a != 49 && a != 57)
+                if (degerBul(Convert.ToByte(a + 7)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a + 7));
+            if (a + 8 < 64)
+                if (degerBul(Convert.ToByte(a + 8)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a + 8));
+            if (a + 9 < 64 && a != 8 && a != 16 && a != 24 && a != 32 && a != 40 && a != 48 && a != 56 && a != 64)
+                if (degerBul(Convert.ToByte(a + 9)) == 0)
+                    ÇevredekiSıfırlar.Add(Convert.ToByte(a + 9));
+
+            return (ÇevredekiSıfırlar);
+
+        }
+
+        void tümSıfırlarıAç(byte a)
+        {
+            List<byte> liste1 = new List<byte>();
+            List<byte> liste2 = new List<byte>();
+
+            liste1 = sıfırAç(a);
+            for (int i = 0; i < 3; i++)
+            {
+                foreach (var item in liste1)
+                {
+                    foreach (var b in sıfırAç(item))
+                    {
+                        liste2.Add(b);
+                    }
+                }
+
+                foreach (var item in liste2)
+                {
+                    foreach (var b in sıfırAç(item))
+                    {
+                        liste1.Add(b);
+                    }
+                }
+            }
+
+        }
+
+        private void Duğmeler_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (btn.BackColor != Color.Red)
+                {
+                    byte a = Convert.ToByte(Convert.ToInt16(btn.Name) + 1);
+
+                    if (degerBul(a) == 255)
+                    {
+                        MayınaBastın();
+                    }
+
+                    if (degerBul(a) == 0)
+                    {
+                        tümSıfırlarıAç(a);
+                    }
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (btn.BackColor != Color.Red)
+                    btn.BackColor = Color.Red;
+                else
+                    btn.BackColor = Color.Aqua;
+
+            }
+
+
+        }
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (Application.OpenForms[0] == this)//Uygulamanin main form'u
+            {
+                //uygulamanin ana formunu kapatirsaniz uygulama kapanir, ana formu yeniden baslatmak uygulamayi yeniden baslatmak demektir.
+                Application.Restart();
+            }
+            else
+            {
+                Form2 f = new Form2();
+                f.Show();
+                this.Close();
+            }
+        }
+
+    }
+}
